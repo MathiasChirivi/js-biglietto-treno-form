653d0d7d46d2e48dc27b8f8baeaa19e4aba4170d
MathiasChirivi <chirivimathias3@gmail.com>
2023-04-15 17:05:24 +0200

    esercizio finito

--- js/main.js
+++ js/main.js
@@ -1,13 +1,5 @@
 
-//let bigliettoCostKm = 0.21;
-//let NomeCognome = document.getElementById("NomePasseggero");
-//let ButtonGenera = document.getElementById("genresButton");
-//let Offerta = document.querySelector("offertaRicevuta");
-//let Carrozza = document.querySelector("CarrozzaRisiedente");
-//let codiceCp = document.querySelector("CodiceRicevuto");
-//let CostoTicket = document.querySelector("PrezzoBiglietto");
 
-//
 genresButton.addEventListener(`click`,
     function(){
         var NomeCognome = document.getElementById("name");
@@ -22,17 +14,42 @@ genresButton.addEventListener(`click`,
         var FasciaEtaPass = FasciaEta.value;
         console.log(FasciaEtaPass);
 
-       var Offerta = 'Tariffa';
+       var Offerta = 'Tariffa Standard';
 
+       let bigliettoCostKm = 0.21;
+       let PrezzoBiglietto = bigliettoCostKm * KilometriPercorrere;
+       console.log(PrezzoBiglietto)
 
        if (FasciaEtaPass == 'Under'){
-
+        PrezzoBiglietto = PrezzoBiglietto - ((PrezzoBiglietto * 20) / 100);
+        Offerta = 'Sconto Minorenne'; 
+        console.log(PrezzoBiglietto);
+       } else if (FasciaEtaPass == 'Over'){
+        PrezzoBiglietto = PrezzoBiglietto - ((PrezzoBiglietto * 40) / 100);
+        Offerta = 'Sconto Over 65'; 
+        console.log(PrezzoBiglietto);
        }
 
         document.getElementById('CodiceRicevuto').innerHTML= Math.floor(Math.random() * 10) + 1;
         document.getElementById('CarrozzaRisiedente').innerHTML = Math.floor(Math.random() * 50) + 1;
         document.getElementById('NomePasseggero').innerHTML = NomePasseggeroCognome;
-        
-
+        document.getElementById('PrezzoBiglietto').innerHTML = PrezzoBiglietto.toFixed(2);
+        document.getElementById('offertaRicevuta').innerHTML = Offerta;
+    
+        document.getElementById('dettagli').className = 'show';
     }
+);
+
+var pulsanteAnnulla = document.getElementById("annulla");
+pulsanteAnnulla.addEventListener('click',
+function() {
+
+    document.getElementById('CodiceRicevuto').innerHTML='';
+    document.getElementById('CarrozzaRisiedente').innerHTML = ''
+    document.getElementById('NomePasseggero').innerHTML = '';
+    document.getElementById('PrezzoBiglietto').innerHTML ='';
+    document.getElementById('offertaRicevuta').innerHTML = '';
+
+  document.getElementById('dettagli').className = 'hidden';
+}
 );
